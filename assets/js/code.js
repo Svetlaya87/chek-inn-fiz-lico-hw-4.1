e3c57362e8edcdfabdfa4b26641e6eba30e0501a
Svetlaya87 <svetlaya87@gmail.com>
2021-06-13 15:47:35 +0300

    2 inputs

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,41 +1,62 @@
+let answer = function (arr) {
+
+    let n10 = ( (-1*arr[0]+5*arr[1]+7*arr[2]+9*arr[3]+4*arr[4]+6*arr[5]+10*arr[6]+5*arr[7]+7*arr[8]) %11) %10;
+
+    if (arr.length != 10){
+        return Result.innerHTML = 'Не правильно введен ИНН. ИНН состоит из 10 чисел';
+
+    }
+
+    //n10=((-1n1+5n2+7n3+9n4+4n5+6n6+10n7+5n8+7n9) mod 11) mod 10
+    else if (arr[9] == n10) {
+
+        if (arr[8] %2 == 0 ){
+            return 'ИНН корректен. Владелец номера-женщина';
+        } else {
+            return 'ИНН корректен. Владелец номера-мужчина';
+        }
+
+    } else {
+        return  'ИНН не корректен.';
+    }
+}
+
+
 
 
 
 function chekInn() {
     let dataFromUser = dataInput.value;
     dataFromUser = dataFromUser.split('');
-    console.log(dataFromUser);
 
-    let n10 = ( (-1*dataFromUser[0]+5*dataFromUser[1]+7*dataFromUser[2]+9*dataFromUser[3]+4*dataFromUser[4]+6*dataFromUser[5]+10*dataFromUser[6]+5*dataFromUser[7]+7*dataFromUser[8]) %11) %10;
-
-    if (dataFromUser.length != 10){
-        Result.innerHTML = 'Не правильно введен ИНН. ИНН состоит из 10 чисел';
-
-    }
-
-    //n10=((-1n1+5n2+7n3+9n4+4n5+6n6+10n7+5n8+7n9) mod 11) mod 10
-    else if (dataFromUser[9] == n10) {
-
-        if (dataFromUser[8] %2 == 0 ){
-            Result.innerHTML = 'ИНН корректен. Владелец номера-женщина';
-        } else {
-            Result.innerHTML = 'ИНН корректен. Владелец номера-мужчина';
-        }
-
-    } else {
-        Result.innerHTML = 'ИНН не корректен.';
-    }
-
+    console.log(`Инн1 ${dataFromUser}`);
+   
     
 
-    
+    Result.innerHTML=`${answer(dataFromUser)}`;
     
 
- 
-
- 
-
-    
+  
 
 }
+
+function chekInn2() {
+    
+    let dataFromUser2 = dataInput2.value;
+    dataFromUser2 = dataFromUser2.split('');
+
+    
+    console.log(`Инн2 ${dataFromUser2}`);
+
+    
+
+    
+    Result2.innerHTML=`${answer(dataFromUser2)}`;
+
+  
+
+}
+
+
+
 
